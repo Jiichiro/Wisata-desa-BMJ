5127b7b744d5a212c2b2a754dfd7419f508b1e2b
Jiichiro <cokmesoh@gmail.com>
2025-02-08 13:24:36 +0700

    fixing robots and sitemaps

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,13 @@
 import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
-  const domain: string = process.env.NEXT_APP_IS_STAGING == 'YES' ?  'localhost:3000' : 'wisatadesa.vercel.app'
+  const domain: string = process.env.NEXT_PUBLIC_IS_STAGING == 'YES' ?  'http://localhost:3000' : 'https://wisatadesa.vercel.app'
   return {
     rules: {
       userAgent: '*',
       allow: '/',
       disallow: '',
     },
-    sitemap:   `https://${domain}/sitemap.xml`,
+    sitemap:   `${domain}/sitemap.xml`,
   }
 }
